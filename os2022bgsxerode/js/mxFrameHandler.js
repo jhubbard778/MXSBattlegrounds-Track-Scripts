357a8bd17e15414dfbdc9caea16620becc4d08ad
Jakob Hubbard <jakobhubbard778@gmail.com>
2022-06-18 03:05:23 -0400

    Race Event Crowd & Frame Handler Updates
    Race event crowd JSON is now sorted by line length, and Frame Handler no longer needs the try catches as everything works as should now.

--- os2022bgsxerode/js/mxFrameHandler.js
+++ os2022bgsxerode/js/mxFrameHandler.js
@@ -1,128 +1,35 @@
 var doneBeginningWork = false;
 function frameHandler(seconds) {
   globalRunningOrder = mx.get_running_order();
+
   if (!doneBeginningWork) {
-    try {
-        resetCurrentTimingGates();
-    }
-    catch (e) {
-        mx.message("First Reset Timing Gates Error: " + e);
-    }
-    try {
-        ResetSlotPositionHolder();
-    }
-    catch (e) {
-        mx.message("First Reset Slot Positions Error: " + e);
-    }
-    try {
-        setUpCheerBooSlots();
-    }
-    catch (e) {
-        mx.message("Set up Cheer and Boo Slots Error: " + e);
-    }
+    resetCurrentTimingGates();
+    ResetSlotPositionHolder();
+    setUpCheerBooSlots();
     doneBeginningWork = true;
   }
 
-  try {
-    updateCamPosition();
-  }
-  catch (e) {
-    mx.message("Update Cam Position Error: " + e);
-  }
-  
+  updateCamPosition();
   if (stadium) updateSoundPositions();
-  try {
-    gateSound();
-  }
-  catch (e) {
-    mx.message("Gate Sound Error: " + e);
-  }
 
-  try {
-    determineHoleshot();
-  }
-  catch (e) {
-    mx.message("Holeshot Error: " + e);
-  }
-  
-  try {
-    updateRiderFinishFlags();
-  }
-  catch (e) {
-    mx.message("Update Rider Finish Flags Error: " + e);
-  }
+  gateSound();
+  determineHoleshot();
+  updateRiderFinishFlags();
   
   if (racingEvent) {
-    try {
-        isRiderDown();
-    }
-    catch (e) {
-        mx.message("Rider Down Error: " + e);
-    }
+    isRiderDown();
+    dynamicMechanicAndFans();
+    battlesFunction();
+    updateRunningOrderScreen();
+    doPyro();
+    riderAwards();
+  }
 
-    try {
-      dynamicMechanicAndFans();
-    }
-    catch (e) {
-      mx.message("Mechanic And Fans Error: " + e);
-    }
-    try {
-        battlesFunction();
-    }
-    catch (e) {
-        mx.message("Battles Error: " + e);
-    }
-    
-    try {
-        updateRunningOrderScreen();
-    }
-    catch (e) {
-        mx.message("Update Running Order Screen Error: " + e);
-    }
-    try {
-        doPyro();
-    }
-    catch (e) {
-        mx.message("Pyro Error: " + e);
-    }
-    
-    try {
-        riderAwards();
-    }
-    catch (e) {
-        mx.message("Awards Error: " + e);
-    }
-  }
-  try {
-	  displayLaptimes();
-  }
-  catch (e) {
-	mx.message("Laptimes Error: " + e);
-  }
-  try {
-    moveBales();
-  }
-  catch (e) {
-    mx.message("Move Bales Error: " + e);
-  }
-  try {
-    timeOrLapsRemaining();
-  }
-  catch (e) {
-    mx.message("Time Or Laps Remaining Error: " + e);
-  }
-  try {
-    flaggersFrameHandler(seconds);
-  }
-  catch (e) {
-    mx.message("Flaggers Error: " + e);
-  }
-  try {
-    resetCurrentTimingGates();
-  }
-  catch (e) {
-    mx.message("Reset Timing Gates Error: " + e);
-  }
+	displayLaptimes();
+  moveBales();
+  timeOrLapsRemaining();
+  flaggersFrameHandler(seconds);
+  resetCurrentTimingGates();
   frameHandlerPrev(seconds);
 }
 
